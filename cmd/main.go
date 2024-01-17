dc390d8a7685ba6762432048e224c0916ce0f446
pelusa-v <razor0164@gmail.com>
2024-01-17 00:39:47 -0500

    Observer is ready to register clients

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,81 @@
 package main
 
-import "github.com/pelusa-v/pelusa-chat.git/internal/chat"
+import (
+	"math/rand"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/pelusa-v/pelusa-chat.git/internal/chat"
+)
 
 func main() {
+	app := fiber.New()
+
 	obs := chat.NewObserver()
 	go obs.Start()
+
+	// registerHandlerWithParams := func(obs *chat.Observer) fiber.Handler {
+	// 	return func(c *fiber.Ctx) error {
+	// 		sampleIds := []string{
+	// 			"697b1579-2186-472f-b636-cfe1a2559bc9",
+	// 			"a19e7ea4-5b09-45fb-b37b-358ebe0e5aa3",
+	// 			"159dce89-dc23-498b-a941-069b7dbbd577",
+	// 			"a9f7b966-8652-477c-9139-a14ca5a19669",
+	// 		}
+	// 		names := []string{
+	// 			"Bob",
+	// 			"Jorge",
+	// 			"Tomy",
+	// 			"Li",
+	// 		}
+
+	// 		id := sampleIds[rand.Intn(len(sampleIds))]
+	// 		name := names[rand.Intn(len(names))]
+	// 		newClient := chat.NewClient(id, name, obs)
+	// 		newClient.Observer.SubscribeClientChan <- newClient
+
+	// 		return c.SendString("A client was added")
+	// 	}
+	// }
+
+	app.Use(func(c *fiber.Ctx) error {
+		c.Locals("observer", obs)
+		return c.Next()
+	})
+
+	app.Post("/register", registerHandler)
+	app.Get("/clients", showClientsHandler)
+	app.Listen(":3000")
+}
+
+func registerHandler(c *fiber.Ctx) error {
+	sampleIds := []string{
+		"697b1579-2186-472f-b636-cfe1a2559bc9",
+		"a19e7ea4-5b09-45fb-b37b-358ebe0e5aa3",
+		"159dce89-dc23-498b-a941-069b7dbbd577",
+		"a9f7b966-8652-477c-9139-a14ca5a19669",
+	}
+	names := []string{
+		"Bob",
+		"Jorge",
+		"Tomy",
+		"Li",
+	}
+
+	obs := c.Locals("observer").(*chat.Observer)
+	id := sampleIds[rand.Intn(len(sampleIds))]
+	name := names[rand.Intn(len(names))]
+	newClient := chat.NewClient(id, name, obs)
+	newClient.Observer.SubscribeClientChan <- newClient
+
+	return c.SendString("A client was added")
+}
+
+func showClientsHandler(c *fiber.Ctx) error {
+	obs := c.Locals("observer").(*chat.Observer)
+	var clients []chat.ClientJson
+	for _, client := range obs.Clients {
+		clients = append(clients, chat.ClientJson{Id: client.Id, Name: client.Name})
+	}
+
+	return c.JSON(clients)
 }
--- internal/chat/client.go
+++ internal/chat/client.go
@@ -1,12 +1,27 @@
 package chat
 
 type Message struct {
-	IdOrigin      string
-	IdDestination string
+	IdOrigin      string `json:"id_origin"`
+	IdDestination string `json:"id_destination"`
+}
+
+type ClientJson struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
 }
 
 type Client struct {
-	Id int
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	Observer *Observer
+}
+
+func NewClient(id string, name string, obs *Observer) *Client {
+	return &Client{
+		Id:       id,
+		Name:     name,
+		Observer: obs,
+	}
 }
 
 func (c *Client) SendMessage() {
